d4d630832d5130380457279f5104074af54c5490
Andrey Tinyakov <akoff16@gmail.com>
2025-03-16 19:21:15 +0300

    revert client

--- internal/app/gophermart/client/client.go
+++ internal/app/gophermart/client/client.go
@@ -4,70 +4,39 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/atinyakov/go-musthave-diploma/internal/app/gophermart/dto"
 	"github.com/atinyakov/go-musthave-diploma/internal/app/gophermart/models"
-	"gopkg.in/eapache/go-resiliency.v1/retrier"
-	retry "gopkg.in/h2non/gentleman-retry.v2"
-	"gopkg.in/h2non/gentleman.v2"
 )
 
 type AccrualClient struct {
-	cli     *gentleman.Client
+	cli     *http.Client
 	baseURL string
 }
 
 func New(baseURL string) *AccrualClient {
-	// Create a new client
-	cli := gentleman.New()
-
-	// Define base URL
-	cli.URL(baseURL)
-
-	// Register the retry plugin, using a custom exponential retry strategy
-	cli.Use(retry.New(retrier.New(retrier.ExponentialBackoff(3, 100*time.Millisecond), nil)))
-
 	return &AccrualClient{
-		cli:     cli,
+		cli:     &http.Client{},
 		baseURL: baseURL,
 	}
 }
 
-type ErrorResponse struct {
-	Message string `json:"message"` // Adjust field based on actual API response
-	Code    int    `json:"code,omitempty"`
-}
-
 func (c *AccrualClient) Request(url string) (*models.Order, error) {
-	// Create a new request based on the current client
-	// c.cli.
-	client := &http.Client{}
-	// пишем запрос
-	// запрос методом POST должен, помимо заголовков, содержать тело
-	// тело должно быть источником потокового чтения io.Reader
+
 	request, err := http.NewRequest(http.MethodGet, c.baseURL+url, nil)
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
 	}
-	// в заголовках запроса указываем кодировку
+
 	request.Header.Add("Content-Type", "application/json")
-	// отправляем запрос и получаем ответ
-	response, err := client.Do(request)
+
+	response, err := c.cli.Do(request)
 	if err != nil {
 		panic(err)
 	}
-	// выводим код ответа
-	fmt.Println("Статус-код ", response.Status)
+
 	defer response.Body.Close()
-	// читаем поток из тела ответа
-	// body, err := io.ReadAll(response.Body)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// // и печатаем его
-	// fmt.Println(string(body))
 
 	var accrual dto.AccrualResponse
 	dec := json.NewDecoder(response.Body)
